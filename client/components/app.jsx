697c9a336376672bae1aabe42ca3d027f5643a2b
adam thibodeaux <athib27@gmail.com>
2021-09-18 16:52:40 -0400

    added add new meal functionality with dummy data

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 // import MealCard from './mealCard.jsx';
 // import GridLayout from 'react-grid-layout';
 // import ReactDom from 'react-dom';
+import Aside from './Aside.jsx';
 import MealDisplay from './MealDisplay.jsx';
 const App = () => {
   return (
     <div>
+      <Aside />
       <MealDisplay />
     </div>
   );
