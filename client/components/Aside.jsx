8f1967469a94d5d826d2e61d1595a74533fb6239
Bernie Green <berniegreen@bernies-air.lan>
2021-09-19 00:25:14 +0800

    built aside component.  updated component names to use proper casing

--- client/components/Aside.jsx
+++ client/components/Aside.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
 const Aside = () => {
+
+    const pantryItems = [{index: 0, name: 'veg'}];
+    const cartItems = [{index: 0, name: 'fruit'}];
+
+    const orderedListBuilder = (item, idx) => {
+    
+        const newItem = <li className='listItem' key={idx} index={idx}>
+            {item.name}
+            </li>
+        return newItem
+    }
+
     return (
-        <div>I'm an aside :&#41; &lt; &gt;</div>
+        <div>
+            <ol className='pantryList'>
+                {pantryItems.map((item, idx) => orderedListBuilder(item, idx))}
+            </ol>
+            <ol className='cartList'>
+                {cartItems.map((item, idx) => orderedListBuilder(item, idx))}
+            </ol>
+        </div>
     );
 };
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, '../client')));
+
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
 });
+
+
 app.listen(3000, () => console.log('listening on port 3000 :)'));
